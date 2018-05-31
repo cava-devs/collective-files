4f8a02862ca983162880126c9607f00369de3f74
Anoop Sownderraj <asownder95@gmail.com>
2018-05-31 07:37:49 -0700

    Package.json / Example data with id, restaurant, and cuisine for standardization.

---
+++ example_data.js
@@ -0,0 +1,405 @@
+const restaurants = [
+  {
+    "rest_id": 0001,
+    "rest_name": 'Cento Osteria',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0002,
+    "rest_name": 'Otoro Sushi',
+    "cuisine": 'Japanese',
+  }, {
+    "rest_id": 0003,
+    "rest_name": 'Monsieur Benjamin',
+    "cuisine": 'French',
+  }, {
+    "rest_id": 0004,
+    "rest_name": 'Rosa Mexicano - San Francisco',
+    "cuisine": 'Mexican',
+  }, {
+    "rest_id": 0005,
+    "rest_name": 'Frascati',
+    "cuisine": 'Mediterranean',
+  }, {
+    "rest_id": 0006,
+    "rest_name": 'Lao Table',
+    "cuisine": 'Thai',
+  }, {
+    "rest_id": 0007,
+    "rest_name": 'Terzo',
+    "cuisine": 'Mediterranean',
+  }, {
+    "rest_id": 0008,
+    "rest_name": 'Mela Tandoori Kitchen',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0009,
+    "rest_name": 'Hayes Street Grill',
+    "cuisine": 'Seafood',
+  }, {
+    "rest_id": 0010,
+    "rest_name": 'Dispensa Italian Charcoal Kitchen',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0011,
+    "rest_name": 'Hashiri',
+    "cuisine": 'Japanese',
+  }, {
+    "rest_id": 0012,
+    "rest_name": 'China Live',
+    "cuisine": 'Chinese',
+  }, {
+    "rest_id": 0013,
+    "rest_name": 'Flores',
+    "cuisine": 'Mexican',
+  }, {
+    "rest_id": 0014,
+    "rest_name": 'FANG',
+    "cuisine": 'Chinese',
+  }, {
+    "rest_id": 0015,
+    "rest_name": 'Jardiniere',
+    "cuisine": 'French',
+  }, {
+    "rest_id": 0016,
+    "rest_name": 'Izakaya Kou',
+    "cuisine": 'Japanese',
+  }, {
+    "rest_id": 0017,
+    "rest_name": 'Amber India - San Francisco',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0018,
+    "rest_name": 'Colibri - Mexican Bistro',
+    "cuisine": 'Mexican',
+  }, {
+    "rest_id": 0019,
+    "rest_name": 'Cadillac Bar & Grill',
+    "cuisine": 'Mexican',
+  }, {
+    "rest_id": 0020,
+    "rest_name": 'Birdsong',
+    "cuisine": 'American',
+  }, {
+    "rest_id": 0021,
+    "rest_name": 'Dirty Habit',
+    "cuisine": 'American',
+  }, {
+    "rest_id": 0022,
+    "rest_name": 'Florio',
+    "cuisine": 'French',
+  }, {
+    "rest_id": 0023,
+    "rest_name": 'Hong Kong Lounge 2',
+    "cuisine": 'Chinese',
+  }, {
+    "rest_id": 0024,
+    "rest_name": 'Zero Zero',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0025,
+    "rest_name": 'Crystal Jade Jiang Nan',
+    "cuisine": 'Chinese',
+  }, {
+    "rest_id": 0026,
+    "rest_name": 'Sens Restaurant',
+    "cuisine": 'Mediterranean',
+  }, {
+    "rest_id": 0027,
+    "rest_name": 'Rocco\'s Cafe',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0028,
+    "rest_name": 'Tawla SF',
+    "cuisine": 'Mediterranean',
+  }, {
+    "rest_id": 0029,
+    "rest_name": 'Troya',
+    "cuisine": 'Mediterranean',
+  }, {
+    "rest_id": 0030,
+    "rest_name": 'Gibson',
+    "cuisine": 'American',
+  }, {
+    "rest_id": 0031,
+    "rest_name": 'Boulette\'s Larder and Boulibar',
+    "cuisine": 'Mediterranean',
+  }, {
+    "rest_id": 0032,
+    "rest_name": 'Scala\'s Bistro',
+    "cuisine": 'French',
+  }, {
+    "rest_id": 0033,
+    "rest_name": 'New Dehli Restaurant',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0034,
+    "rest_name": 'Eight Tables by George Chen',
+    "cuisine": 'Chinese',
+  }, {
+    "rest_id": 0035,
+    "rest_name": 'MKT Restaurant and Bar',
+    "cuisine": 'Seafood',
+  }, {
+    "rest_id": 0036,
+    "rest_name": 'Barbacco',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0037,
+    "rest_name": 'Thai Spice',
+    "cuisine": 'Thai',
+  }, {
+    "rest_id": 0038,
+    "rest_name": 'Revelry Bistro',
+    "cuisine": 'French',
+  }, {
+    "rest_id": 0039,
+    "rest_name": 'Spice of America',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0040,
+    "rest_name": 'Alamo Square Seafood Grill',
+    "cuisine": 'Seafood',
+  }, {
+    "rest_id": 0041,
+    "rest_name": 'SOMA Restaurant & Bar',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0042,
+    "rest_name": 'Okane',
+    "cuisine": 'Japanese',
+  }, {
+    "rest_id": 0043,
+    "rest_name": 'Puccini & Pinetti',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0044,
+    "rest_name": 'Catch - San Francisco',
+    "cuisine": 'Seafood',
+  }, {
+    "rest_id": 0045,
+    "rest_name": 'Chaat Conrner - Union Square',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0046,
+    "rest_name": 'Osha Thai Noodle Cafe on Geary',
+    "cuisine": 'Thai',
+  }, {
+    "rest_id": 0047,
+    "rest_name": 'The Cavalier',
+    "cuisine": 'American',
+  }, {
+    "rest_id": 0048,
+    "rest_name": 'Waterbar',
+    "cuisine": 'Seafood',
+  }, {
+    "rest_id": 0049,
+    "rest_name": 'Chili House',
+    "cuisine": 'Chinese',
+  }, {
+    "rest_id": 0050,
+    "rest_name": 'Omakase',
+    "cuisine": 'Japanese',
+  }, {
+    "rest_id": 0051,
+    "rest_name": 'Z & Y Restaurant',
+    "cuisine": 'Chinese',
+  }, {
+    "rest_id": 0052,
+    "rest_name": 'Gracias Madre - SF',
+    "cuisine": 'Mexican',
+  }, {
+    "rest_id": 0053,
+    "rest_name": 'Delarosa\'s Downtown',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0054,
+    "rest_name": 'Marlowe',
+    "cuisine": 'American',
+  }, {
+    "rest_id": 0055,
+    "rest_name": 'Tratto',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0056,
+    "rest_name": 'Kokkari Estiatorio',
+    "cuisine": 'Mediterranean',
+  }, {
+    "rest_id": 0057,
+    "rest_name": 'Aliment',
+    "cuisine": 'American',
+  }, {
+    "rest_id": 0058,
+    "rest_name": 'Clay Oven on Haight',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0059,
+    "rest_name": 'Kitchen Istanbul',
+    "cuisine": 'Mediterranean',
+  }, {
+    "rest_id": 0060,
+    "rest_name": 'Nomica',
+    "cuisine": 'Japanese',
+  }, {
+    "rest_id": 0061,
+    "rest_name": 'Leo\'s Oyster Bar',
+    "cuisine": 'Seafood',
+  }, {
+    "rest_id": 0062,
+    "rest_name": 'Hawker Fare - SF',
+    "cuisine": 'Thai',
+  }, {
+    "rest_id": 0063,
+    "rest_name": 'Anchor and Hope',
+    "cuisine": 'Seafood',
+  }, {
+    "rest_id": 0064,
+    "rest_name": 'Tres - SF',
+    "cuisine": 'Mexican',
+  }, {
+    "rest_id": 0065,
+    "rest_name": 'Pera',
+    "cuisine": 'Mediterranean',
+  }, {
+    "rest_id": 0066,
+    "rest_name": 'El Buen Comer',
+    "cuisine": 'Mexican',
+  }, {
+    "rest_id": 00067,
+    "rest_name": 'Urban Tavern',
+    "cuisine": 'American',
+  }, {
+    "rest_id": 0068,
+    "rest_name": 'Fog Harbor Fish House',
+    "cuisine": 'Seafood',
+  }, {
+    "rest_id": 0069,
+    "rest_name": 'Kasa Indian Eatery',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0070,
+    "rest_name": 'The Keystone',
+    "cuisine": 'American',
+  }, {
+    "rest_id": 0071,
+    "rest_name": 'Ozumo - San Francisco',
+    "cuisine": 'Japaenese',
+  }, {
+    "rest_id": 0072,
+    "rest_name": 'Besharam',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0073,
+    "rest_name": '54 Mint',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0074,
+    "rest_name": 'Farmhouse Kitchen Thai Cuisine',
+    "cuisine": 'Thai',
+  }, {
+    "rest_id": 0075,
+    "rest_name": 'Roy\'s - San Francisco',
+    "cuisine": 'Seafood',
+  }, {
+    "rest_id": 0076,
+    "rest_name": 'Mathilde Bistro',
+    "cuisine": 'French',
+  }, {
+    "rest_id": 0077,
+    "rest_name": 'Mikaku Restaurant',
+    "cuisine": 'Japanese',
+  }, {
+    "rest_id": 0078,
+    "rest_name": 'Per Diem',
+    "cuisine": 'American',
+  }, {
+    "rest_id": 0079,
+    "rest_name": 'Jai Yun',
+    "cuisine": 'Chinese',
+  }, {
+    "rest_id": 0080,
+    "rest_name": 'Roti Indian Bistro - San Francisco',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0081,
+    "rest_name": 'Arguello',
+    "cuisine": 'Mexican',
+  }, {
+    "rest_id": 0082,
+    "rest_name": 'Truly Mediterranean',
+    "cuisine": 'Mediterranean',
+  }, {
+    "rest_id": 0083,
+    "rest_name": 'Curbside Cafe',
+    "cuisine": 'French',
+  }, {
+    "rest_id": 0084,
+    "rest_name": 'Izakaya Roku',
+    "cuisine": 'Japanese',
+  }, {
+    "rest_id": 0085,
+    "rest_name": 'Aslam\'s Rasoi',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0086,
+    "rest_name": 'Black Cat',
+    "cuisine": 'American',
+  }, {
+    "rest_id": 0087,
+    "rest_name": 'Bouche',
+    "cuisine": 'French',
+  }, {
+    "rest_id": 0088,
+    "rest_name": 'La Briciola',
+    "cuisine": 'Italian',
+  }, {
+    "rest_id": 0089,
+    "rest_name": 'Akiko\'s Restaurant & Sushi Bar',
+    "cuisine": 'Japanese',
+  }, {
+    "rest_id": 0090,
+    "rest_name": 'Cocotte',
+    "cuisine": 'French',
+  }, {
+    "rest_id": 0091,
+    "rest_name": 'Hanlin Tea House & Restaurant',
+    "cuisine": 'Chinese',
+  }, {
+    "rest_id": 0092,
+    "rest_name": 'Saffron 685 Mediterranean Turkish Cuisine',
+    "cuisine": 'Mediterranean',   
+  }, {
+    "rest_id": 0093,
+    "rest_name": 'PABU - San Francisco',
+    "cuisine": 'Japanese',
+  }, {
+    "rest_id": 0094,
+    "rest_name": 'Sabrosa',
+    "cuisine": 'Mexican',
+  }, {
+    "rest_id": 0095,
+    "rest_name": 'Regalito Rosticeria',
+    "cuisine": 'Mexican',
+  }, {
+    "rest_id": 0096,
+    "rest_name": 'Farallon',
+    "cuisine": 'Seafood',
+  }, {
+    "rest_id": 0097,
+    "rest_name": 'Urban Curry',
+    "cuisine": 'Indian',
+  }, {
+    "rest_id": 0098,
+    "rest_name": 'Brandy Ho\'s Hunan Food',
+    "cuisine": 'Chinese',
+  }, {
+    "rest_id": 0099,
+    "rest_name": 'Le Central',
+    "cuisine": 'French',
+  }, {
+    "rest_id": 0100,
+    "rest_name": 'LaLe Mediterranean & Turkish Restaurant',
+    "cuisine": 'Mediterranean',
+  }
+
+
+];
